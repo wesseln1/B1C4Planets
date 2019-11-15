27a413964d07bd1a059d19150f830e2ba25b8936
unknown <nwessel429@gmail.com>
2019-11-15 13:17:25 -0600

    added mulitple planet lists

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Planet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> lastPlanets = new List<string>()
+            {
+                "Uranus",
+                "Neptune"
+            };
+
+            planetList.AddRange(lastPlanets);
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+            planetList.Add("Pluto");
+
+            List<string> rockyPlanets = planetList.GetRange(0, 4);
+            planetList.Remove("Pluto");
+        }
+    }
+}
